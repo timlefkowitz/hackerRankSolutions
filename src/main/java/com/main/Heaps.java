ae7e9b70ff4d6cded4b8d0645e1fede5103974d1
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-03-29 20:31:38 -0500

    building a heap

--- src/main/java/com/main/Heaps.java
+++ src/main/java/com/main/Heaps.java
@@ -3,4 +3,17 @@ public class MinIntHeap {
     private int size = 0;
 
     int[] items = new int[capacity];
+
+    private int getLeftChildIndex(int parentIndex) {return 2 * parentIndex +1;}
+    private int getRightChildIndex(int parentIndex) {return 2 * parentIndex +1;}
+    private int getParentIndex(int childIndex) {return (childIndex - 1) / 2;}
+
+    private boolean hasLeftChild (int index) {return getLeftChildIndex(index) < size;}
+    private boolean hasRightChild (int index) {return getRightChildIndex(index) < size;}
+    private boolean hasParent (int index) {return getParentIndex(index) >=0;}
+
+    private int leftChild(int index){return items [getLeftChildIndex(index)];}
+    private int rightChild(int index){return items [getRightChildIndex(index)];}
+    private int leftChild(int index){return items [getParentIndex(index)];}
+
 }
