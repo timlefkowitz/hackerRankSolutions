5244825ac0885cc6d39c6357f71453a3e6bd5ce9
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-05-10 00:39:48 -0500

    sol3

--- src/main/java/com/main/hrsolutions/HackerRankSolutions.java
+++ src/main/java/com/main/hrsolutions/HackerRankSolutions.java
@@ -289,4 +289,27 @@ Sample Case 1:
             System.out.println("Weird");
         } 
     }
+
+
+    public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+ 
+       
+        
+
+        // Write your code here.
+        scan.nextLine();
+        String s = scan.nextLine();
+        Double d = scan.nextDouble();
+        int i = scan.nextInt();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
+
 }
