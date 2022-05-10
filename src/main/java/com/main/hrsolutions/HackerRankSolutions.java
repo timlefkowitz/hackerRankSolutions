216ea1a414cab1794aeb0eef687bfb7dd9b678cf
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-05-09 22:20:20 -0500

    sol3

--- src/main/java/com/main/hrsolutions/HackerRankSolutions.java
+++ src/main/java/com/main/hrsolutions/HackerRankSolutions.java
@@ -226,3 +226,67 @@ Hello, Java. */
 }
 
 }
+/***In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes. An if-else statement has the following logical flow:
+
+Wikipedia if-else flow chart
+
+Source: Wikipedia
+
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+Weird
+Sample Input 1
+
+24
+Sample Output 1
+
+Not Weird
+Explanation
+
+Sample Case 0: 
+ is odd and odd numbers are weird, so we print Weird.
+
+Sample Case 1: 
+ and  is even, so it isn't weird. Thus, we print Not Weird.
+ 
+  */
+
+
+// If  is odd, print Weird
+        if(N % 2 == 1){
+            System.out.println("Weird");
+            
+        } else if(N % 2 == 0 && N > 20){
+            System.out.println("Not Weird");
+            // If N is even and in the inclusive range of 2 to 5, print Not Weird
+        } else if (N % 2 == 0 && N <= 5 && N >= 2){
+            System.out.println("Not Weird");
+
+            // If N is even and in the inclusive range of 6 to 20, print Weird
+        } else if (N % 2 == 0 && N <= 20 && N >= 6 ){
+            System.out.println("Weird");
+        } 
+    }
+}
