27faa9ae6a8feaef27e388a9ec11f95e52e5802d
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-05-12 21:20:03 -0500

    sol3

--- src/main/java/com/main/hrsolutions/HackerRankSolutions.java
+++ src/main/java/com/main/hrsolutions/HackerRankSolutions.java
@@ -312,4 +312,41 @@ Sample Case 1:
     }
 }
 
+
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+        
+
+
+
+// If N is even and greater than 20, print Not Weird
+
+// If  is odd, print Weird
+        if(N % 2 == 1){
+            System.out.println("Weird");
+            
+        } else if(N % 2 == 0 && N > 20){
+            System.out.println("Not Weird");
+            // If N is even and in the inclusive range of 2 to 5, print Not Weird
+        } else if (N % 2 == 0 && N <= 5 && N >= 2){
+            System.out.println("Not Weird");
+
+            // If N is even and in the inclusive range of 6 to 20, print Weird
+        } else if (N % 2 == 0 && N <= 20 && N >= 6 ){
+            System.out.println("Weird");
+        } 
+    }
+}
+
+
+
 }
